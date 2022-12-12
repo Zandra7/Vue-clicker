af17668e804651fca75c5164dc794be567504166
Sandra Eng <sandra.eng@live.no>
2022-12-12 09:37:48 +0100

    Click the cookie

--- clicker/src/App.vue
+++ clicker/src/App.vue
@@ -7,7 +7,7 @@
   <h2>Cookies: {{count}}</h2>
   <h2>Cash: {{money}}$</h2>
 
-  <button @click="click()">Get cookies</button>
+  <img @click="click()" src="src/assets/cartoonCookie.png" alt="cookie to click">
   <br>
   <button @click="sell1()">Sell 1</button>
   <button @click="sell10()">Sell 10</button>
@@ -15,6 +15,9 @@
   <br>
   <button @click="sellall()">Sell all</button>
   <br>
+  <br>
+  <h3>Cost: {{cookieIncrease*20}}$</h3>
+  <button @click="click2()">+1 Click</button>
   <br>
   <h3>Cost: 150$</h3>
   <button @click="buyAutocl()">Buy autoclicker</button>
@@ -32,40 +35,46 @@ export default {
       count: 0,
       money: 0,
       check: null,
-
+      cookieIncrease: 1,
     }
   },
   methods: {
     click(){
-      this.count++
+      this.count += this.cookieIncrease
+    },
+    click2(){
+      if (this.money >= this.cookieIncrease*20){
+        console.log("hei")
+        this.money -= this.cookieIncrease*20
+        this.cookieIncrease++
+      }
     },
     sell1(){
-      if (this.count > 0){
-        console.log("1")
-        this.money+=1*2
+      if (this.count >= 0){
+        this.money+=1
         this.count-=1
       }
     },
     sell10(){
       if(this.count > 9){
-        this.money+=10*2
+        this.money+=10
         this.count-=10
       }
     },
     sell100(){
       if(this.count > 99){
-        this.money+=100*2
+        this.money+=100
         this.count-=100
       }
     },
     sellall(){
       if(this.count > 0){
-        this.money+=this.count*2
+        this.money+=this.count
         this.count-=this.count
       }
     },
     buyAutocl(){
-      if(this.money > 1) {
+      if(this.money >= 150) {
         this.money -= 150
         this.check = setInterval(this.addCookies,1000);  
       }
@@ -74,7 +83,8 @@ export default {
       if(this.check != null) {
         clearInterval(this.check)
         this.check = null
-        
+        this.money += 150
+
       }
     },
     addCookies(){
@@ -88,19 +98,24 @@ export default {
   .app {
     text-align: center;
   }
+
   .title {
   margin-top: 2%;
 }
 
+  img {
+    height: 200px;  
+  }
   button {
     font-size: 20px;
     padding:  10px 20px;
     margin-bottom: 1%;
-    border: 1px solid #ccc;
+    border: 1px solid rgb(0, 0, 0);
     border-radius: 5px;
-    background: #fff;
+    background: rgba(215, 153, 94, 0.969);
     cursor: pointer;
     width: 200px;
     height: 6%;
+    color: rgb(65, 36, 10)
   }
 </style>
